979deeb0bb44f001c787ca39d5c3c7f8b309f710
ahbarnett <ahb@math.dartmouth.edu>
2019-05-31 17:05:08 -0400

    README doc and compile script allowing env var override

--- compileFFTWBenchmark.sh
+++ compileFFTWBenchmark.sh
@@ -1,1 +1,9 @@
-g++  -o fftwBenchmark -fopenmp fftwBenchmark.cpp ~/work/finufft/src/utils.cpp -I /usr/include -I ~/work/finufft/src -I /usr/lib/gcc/x86_64-linux-gnu/8/include -L /usr/lib/x86_64-linux-gnu -L ~/work/finufft/lib/ -lfftw3_omp -lfftw3 -lfinufft -lgomp -lm
+#!/bin/bash
+
+# User to set environment variable FINUFFT to point to that repo's main dir.
+# Otherwise this default is used:
+if [ -z "$FINUFFT" ]; then
+    FINUFFT=~/work/finufft
+fi
+
+g++-8  -o fftwBenchmark fftwBenchmark.cpp $FINUFFT/src/utils.cpp -I /usr/include -I $FINUFFT/src/ -I /usr/lib/gcc/x86_64-linux-gnu/8/include -L /usr/lib/x86_64-linux-gnu -L $FINUFFT/lib/ -lfftw3_omp -lfftw3 -lfinufft -fopenmp -lm
